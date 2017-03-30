942382b10f8f5cab3637afb8d2dd30b539abf976
Spencer Alessi <alessisb@gmail.com>
2017-03-29 20:34:10 -0400

    Added DNS lookup for Running VMs
    Added a DNS lookup for Running VMs instead of relying on the Xymon hosts.cfg file to give me the name or on the VM name which isn't necessarily the DNS name. Xymon gets confused when the name in the hosts.cfg file doesn't match the DNS name of the server being monitored. Also cleaned up some spacing & indenting.

--- snapshots.ps1
+++ snapshots.ps1
@@ -46,39 +46,52 @@ if ($ENV.StartsWith("development")) {
 Connect-VIServer $server -user $USER -password $PASS
 
 foreach ($vm in (get-vm)) {
-$output = ""
-$vmname = $VM.name.ToUpper()
-$snapshot = get-vm -Name $vm.name | get-snapshot
+	$output = ""
+	$vmname = ""
 
-if (($snapshot.Length) -gt 0) {
-	foreach ($snap in $snapshot) {
+	$vmguest = Get-VMGuest -vm $vm.name
+
+	# If the VM is not running we won't be able to grab DNS Name
+	try {
+		$vmname = $vmguest.HostName.split('.')[0]
+		$vmname = $vmname.ToUpper()
+	} catch {
+		BREAK
+	}
+	
+	# Get the snapshots
+	$snapshot = get-vm -Name $vm.name | get-snapshot
+
+	if (($snapshot.Length) -gt 0) {
+		foreach ($snap in $snapshot) {
 			# Output
 			$output += "`r`n - " + $snap.created + " - \`"$($snap.name)\`""
-	}
-	$snapinfo = "`r`n" + "ESXi Server: $server" + "`r`n" + "Virtual Machine: $vmname" + "`r`n" + "Snapshot(s):" + $output + "`r`n"
-	
-	# Change the color of the icon based on snapshot age
-	$THEDATE = Get-Date
-	$REDDATE = $THEDATE.AddDays(-$RED)
-	$YELLOWDATE = $THEDATE.AddDays(-$YELLOW)
-
-	#If the snapshot(s) are older than or equal to $RED Days
-	if ($snapshot.created -le $REDDATE) {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots red Snapshot(s) Found $snapinfo`r`n(Red >= $RED days old)" > /dev/null 2>&1
-	}
-	#If the snapshot(s) are older than or equal to $YELLOW Days
-	elseif ($snapshot.created -le $YELLOWDATE) {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots yellow Snapshot(s) Found $snapinfo`r`n(Yellow >= $YELLOW days old)" > /dev/null 2>&1
-	}
-	#If the snapshot(s) are less than $YELLOW Days
-	else {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green Snapshot(s) Found $snapinfo`r`n(Green < $YELLOW days old)" > /dev/null 2>&1
-	}
-}
-# Otherwise no snapshots were found
-else {
-				$snapinfo = "No snapshots found"
-				xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green $snapinfo"> /dev/null 2>&1
 		}
+	
+		$snapinfo = "`r`n" + "ESXi Server: $server" + "`r`n" + "Virtual Machine: $vmname" + "`r`n" + "Snapshot(s):" + $output + "`r`n"
+	
+		# Change the color of the icon based on snapshot age
+		$THEDATE = Get-Date
+		$REDDATE = $THEDATE.AddDays(-$RED)
+		$YELLOWDATE = $THEDATE.AddDays(-$YELLOW)
+
+		#If the snapshot(s) are older than or equal to $RED Days
+		if ($snapshot.created -le $REDDATE) {
+			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots red Snapshot(s) Found $snapinfo`r`n(Red >= $RED days old)" > /dev/null 2>&1
+		}
+		#If the snapshot(s) are older than or equal to $YELLOW Days
+		elseif ($snapshot.created -le $YELLOWDATE) {
+			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots yellow Snapshot(s) Found $snapinfo`r`n(Yellow >= $YELLOW days old)" > /dev/null 2>&1
+		}
+		#If the snapshot(s) are less than $YELLOW Days
+		else {
+			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green Snapshot(s) Found $snapinfo`r`n(Green < $YELLOW days old)" > /dev/null 2>&1
+		}
+	}
+	# Otherwise no snapshots were found
+	else {
+		$snapinfo = "No snapshots found"
+		xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green $snapinfo"> /dev/null 2>&1
+	}
 }
 

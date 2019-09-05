4d9f8627b0c25a91dfdab8c47be8d5c3beccd5a4
Spencer Alessi <alessisb@gmail.com>
2019-09-05 13:15:03 -0400

    updated snapshots.ps1

--- snapshots.ps1
+++ snapshots.ps1
@@ -1,24 +1,27 @@
 <#
 .SYNOPSIS
-	Gets snapshot information from ESXI and posts it to Xymon.
+        Gets snapshot information from ESXI and posts it to Xymon.
 .DESCRIPTION
-	Queries the VMware ESXI Environment based on the host that is running the script.
-	Gathers snapshot information such as Snapshot Name and Creation Date.
-	Creates a new column in Xymon and depending on the age of the snapshot
-	the icon will change to Red, Yellow or Green.
+        Queries the VMware ESXI Environment based on the host that is running the script.
+        Gathers snapshot information such as Snapshot Name and Creation Date.
+        Creates a new column in Xymon and depending on the age of the snapshot
+        the icon will change to Red, Yellow or Green.
 .NOTES
-	File Name      : snapshots.ps1
-	Author         : Spencer Alessi
-   	Prerequisite   : Ubuntu 14.04.3 x64 | PowerShell Version 6.0.0-alpha.16-1ubuntu1.14.04.1 | VMware PowerCLI Version 1.0 | Xymon 4.3.17
-	Date	       : March 27th 2017
+        File Name      : snapshots.ps1
+        Author         : Spencer Alessi
+        Prerequisite   : Ubuntu 18.04 x64, Xymon 4.3.17, PowerShell Core, VMware PowerCLI
+        Date           : September 5th 2019
 .LINK
-	- PowerShell https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md
-	- VMWare PowerCLI https://labs.vmware.com/flings/powercli-core
-	- Getting Started with PowerCLI http://www.virten.net/2016/10/getting-started-with-powercli-for-linux-powercli-core/
+        - Installing Powershell Core on Linux
+        https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=power$
+
+        - Install PowerCLI on Ubuntu 18.04
+        https://www.altaro.com/vmware/install-powercli-ubuntu-linux-18-04-lts/)
+
 #>
 
 # Import all the PowerCLI Modules
-Get-Module -ListAvailable PowerCLI* | Import-Module
+Get-Module -ListAvailable "VMware.PowerCLI" | Import-Module
 
 # Date
 $THEDATE = Get-Date
@@ -36,15 +39,15 @@ $DELAY = "+20h"
 
 # Xymon Enviornment
 $ENV = hostname
-$USER = "mruser"
-$PASS = "my really cool password"
+$USER = "xxx"
+$PASS = "xxx"
 
-if ($ENV.StartsWith("development")) {
-        $server = "thedevelopmentserver"
+if ($ENV.StartsWith("dev")) {
+        $server = "development"
 } elseif ($ENV.StartsWith("test")) {
-        $server = "thetestserver"
-} elseif ($ENV.StarsWith("production")) {
-        $server = "theproductionserver"
+        $server = "test"
+} elseif ($ENV.StartsWith("prod")) {
+        $server = "production"
 } else {
         # die
 }
@@ -53,62 +56,62 @@ if ($ENV.StartsWith("development")) {
 Connect-VIServer $server -user $USER -password $PASS
 
 foreach ($vm in (get-vm)) {
-	$output = ""
-	$vmname = ""
+        $output = ""
+        $vmname = ""
 
-	$vmguest = Get-VMGuest -vm $vm.name
-	
-	# If the VM is not running we won't be able to grab DNS Name
-	try {
-		$vmname = $vmguest.HostName.split('.')[0]
-		$vmname = $vmname.ToUpper()
-	} catch {
-		# continue
-	}
-	
-	# Get the snapshots
-	$snapshot = get-vm -Name $vm.name | get-snapshot
+        $vmguest = Get-VMGuest -vm $vm.name
 
-	if (($snapshot.Length) -gt 0) {
-		foreach ($snap in $snapshot) {
-			# Snapshot size
-			$snapshotsize = [math]::Round($snap.SizeGB,2)
+        # If the VM is not running we won't be able to grab DNS Name
+        try {
+                $vmname = $vmguest.HostName.split('.')[0]
+                $vmname = $vmname.ToUpper()
+        } catch {
+                # continue
+        }
 
-			# Snapshot Creation Date
-			$created = $snap.created
-			
-			$daysold = ($THEDATE - $created).days
-			$hoursold = [math]::Round(($THEDATE - $created).hours / 24,2)
-			$age = "$($daysold + $hoursold) Days(s)"
+        # Get the snapshots
+        $snapshot = get-vm -Name $vm.name | get-snapshot
+
+        if (($snapshot.Length) -gt 0) {
+                foreach ($snap in $snapshot) {
+                        # Snapshot size
+                        $snapshotsize = [math]::Round($snap.SizeGB,2)
+
+                        # Snapshot Creation Date
+                        $created = $snap.created
+
+                        $daysold = ($THEDATE - $created).days
+                        $hoursold = [math]::Round(($THEDATE - $created).hours / 24,2)
+                        $age = "$($daysold + $hoursold) Days(s)"
 
 
-			# Snapshot output
-			$output += "`r`n - " + $snap.created + " - \`"$($snap.name)\`"" + " - " + $snapshotsize + "GB" + " - " + $age + ""
+                        # Snapshot output
+                        $output += "`r`n - " + $snap.created + " - \`"$($snap.name)\`"" + " - " + $snapshotsize + "G$
 
-		}
+                }
 
-		$output
-		$snapinfo = "`r`n" + "ESXi Server: $server" + "`r`n" + "Virtual Machine: $vmname" + "`r`n" + "Snapshot(s):`r`n" + $output + "`r`n"
-		
-		
+                $output
+                $snapinfo = "`r`n" + "ESXi Server: $server" + "`r`n" + "Virtual Machine: $vmname" + "`r`n" + "Snapsh$
 
-		#If the snapshot(s) are older than or equal to $RED Days
-		if ($snapshot.created -le $REDDATE) {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots red Snapshot(s) Found $snapinfo`r`n(Red >= $RED days old)" > /dev/null 2>&1
-		}
-		#If the snapshot(s) are older than or equal to $YELLOW Days
-		elseif ($snapshot.created -le $YELLOWDATE) {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots yellow Snapshot(s) Found $snapinfo`r`n(Yellow >= $YELLOW days old)" > /dev/null 2>&1
-		}
-		#If the snapshot(s) are less than $YELLOW Days
-		else {
-			xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green Snapshot(s) Found $snapinfo`r`n(Green < $YELLOW days old)" > /dev/null 2>&1
-		}
-	}
-	# Otherwise no snapshots were found
-	else {
-		$snapinfo = "No snapshots found"
-		xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green $snapinfo"> /dev/null 2>&1
-	}
+
+
+                #If the snapshot(s) are older than or equal to $RED Days
+                if ($snapshot.created -le $REDDATE) {
+                        xymon 127.0.0.1 "status${DELAY} $vmname.snapshots red Snapshot(s) Found $snapinfo`r`n(Red >=$
+                }
+                #If the snapshot(s) are older than or equal to $YELLOW Days
+                elseif ($snapshot.created -le $YELLOWDATE) {
+                        xymon 127.0.0.1 "status${DELAY} $vmname.snapshots yellow Snapshot(s) Found $snapinfo`r`n(Yel$
+                }
+                #If the snapshot(s) are less than $YELLOW Days
+                else {
+                        xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green Snapshot(s) Found $snapinfo`r`n(Gree$
+                }
+        }
+        # Otherwise no snapshots were found
+        else {
+                $snapinfo = "No snapshots found"
+                xymon 127.0.0.1 "status${DELAY} $vmname.snapshots green $snapinfo" | out-null
+        }
 }
 
